516e733c3565902c0e6b1d95fc7794f9a5586b70
Gilmar Freire <gsfreire33@gmail.com>
2024-03-10 00:16:45 -0300

    Atualização do projeto

--- app.js
+++ app.js
@@ -1,18 +1,24 @@
 var inputText = document.querySelector("#text-area-input");
 var outInput_text = document.querySelector("#exit_messages2");
 
-function teste(){
+// As "chaves" de criptografia que utilizaremos são:
+// `A letra "e" é convertida para "enter"`
+// `A letra "i" é convertida para "imes"`
+// `A letra "a" é convertida para "ai"`
+// `A letra "o" é convertida para "ober"`
+// `A letra "u" é convertida para "ufat"`
 
-    
-   // outInput_text.value = inputText.value
-    // inputText.value=""
+function teste(){
 
     var texto = inputText.value;
     inputText.value=""
 
-    document.getElementById('exit_messages2').innerHTML = '<textarea readonly id="text-area-input">' + texto + 
-  '</textarea>' + '<button class="btn-copiar" id="copiar" onclick="copiar()">Copiar</button>'
+    var resultCripto = texto.replace(/e/g, "enter").replace(/i/g, "imes").replace(/a/g, "ai").replace(/o/g, "ober").replace(/u/g, "ufat")
 
 
+    exit_messages2.value = resultCripto
+
+    document.getElementById('exit_messages2').innerHTML = '<textarea readonly class="exit_messages3">' + resultCripto + 
+  '</textarea>' + '<button class="action-button-btn"  id="copiar" onclick="copiar()">Copiar</button>' 
   
   }
